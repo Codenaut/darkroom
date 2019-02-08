76a7b66375962fb7675a81f203d475370f02262e
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2019-02-08 11:54:40 +0100

    remove dummy action

--- main.go
+++ main.go
@@ -56,11 +56,6 @@ func main() {
 		},
 	}
 
-	app.Action = func(c *cli.Context) error {
-		log.Printf("Hello %q", c.Args().Get(0))
-		return nil
-	}
-
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
